a65aa883860e893fd42a60e4b00dfe9d7d1fb959
Chris <chris211321@gmail.com>
2018-07-10 17:39:48 -0400

    remove window tests

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom'
 import Root from './components/root'
 import configureStore from './store/store'
 
-// TEST IMPORTS
-import { signup, logout, login } from './actions/session_actions'
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
@@ -23,10 +20,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const root = document.getElementById('root')
   ReactDOM.render(<Root store={store} />, root)
-
-  // TESTS
-  window.store = store
-  window.signup = signup
-  window.logout = logout
-  window.login = login
 })
