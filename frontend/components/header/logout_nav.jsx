6f7b4e2331cb189114fe868cd283b1e86845827a
Chris <chris211321@gmail.com>
2018-07-10 17:57:45 -0400

    restore header img tag

--- frontend/components/header/logout_nav.jsx
+++ frontend/components/header/logout_nav.jsx
@@ -28,6 +28,9 @@ class LogoutNav extends React.Component {
           Sign Out
         </button>
 
+        <img
+          className="header-image"
+          src={this.props.currentUser.avatar} />
       </nav>
     )
   }
