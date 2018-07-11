99400c8e9acf14bfef45637af4a9f3475cce6dc4
Chris <chris211321@gmail.com>
2018-07-11 15:47:29 -0400

    add links to MainIndexItems

--- frontend/components/main/main_index_item.jsx
+++ frontend/components/main/main_index_item.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const MainIndexItem = ({ story, author,  }) => {
+  const storyUrl = `/stories/${story.id}`
+  const authorUrl = `/users/${author.id}`
   return (
     <div className="main-index-item">
       <div className="main-index-item-content">
         <div className="item-story">
-          <h1 className="item-story-title">{story.title}</h1>
-          <h1 className="item-story-subtitle">{story.subtitle}</h1>
-          <h1 className="item-story-author">{author.name}</h1>
+          <Link to={storyUrl}>
+            <h1 className="item-story-title">{story.title}</h1>
+            <h1 className="item-story-subtitle">{story.subtitle}</h1>
+          </Link>
+          <Link to={authorUrl}>
+            <h1 className="item-story-author">{author.name}</h1>
+          </Link>
         </div>
         <div className="item-data">
           <h1 className="item-data-date">{story.date}</h1>
