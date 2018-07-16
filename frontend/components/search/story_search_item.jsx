2702b5adbaadc269cce3c38bcd1f828cb8d2f0dc
Chris <chris211321@gmail.com>
2018-07-16 13:32:44 -0400

    add links to search items

--- frontend/components/search/story_search_item.jsx
+++ frontend/components/search/story_search_item.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default ({ story }) => {
+  
+  const storyURL = `/stories/${story.id}`
+
   return (
-    <div className="search-item">
+    <Link 
+      className="search-item"
+      to={storyURL} >
       <div className="search-item-left">
         <h1 className="search-item-title">{story.title}</h1>
         <h1 className="search-item-subtitle">{story.subtitle}</h1>
       </div>
-    </div>
+    </Link>
   )
 }
--- frontend/components/search/user_search_item.jsx
+++ frontend/components/search/user_search_item.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default ({ user }) => {
+
+  const userURL = `/users/${user.id}`
+
   return (
-    <div className="search-item">
+    <Link 
+      className="search-item"
+      to={userURL} >
       <div className="search-item-left">
         <h1 className="search-item-title">{user.name}</h1>
         <h1 className="search-item-subtitle">{user.bio}</h1>
@@ -10,6 +16,6 @@ export default ({ user }) => {
       <img
         className="search-item-right"
         src={user.avatar} />
-    </div>
+    </Link>
   )
 }
