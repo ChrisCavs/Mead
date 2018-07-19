b668ae06d29e8d9e494dc8c7b5c2605681b0c26f
Chris <chris211321@gmail.com>
2018-07-19 11:55:39 -0400

    fix image styles on phone screen

--- frontend/components/header/new_button.jsx
+++ frontend/components/header/new_button.jsx
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom'
 
 export default ({addedClass}) => {
   const classes = `header-button new-button ${addedClass}`
+  let text = 'Write a story'
+  if (addedClass === 'right') {
+    text = <img src={window.editImage} />
+  }
   return (
     <Link
       className={classes}
       to='/stories/new'>
-      Write a story
+      {text}
     </Link>
   )
 }
